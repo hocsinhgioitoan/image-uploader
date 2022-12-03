54713b09037f86c9788ed3b4c5f2486c527adf31
hocsinhgioitoan <89916615+hocsinhgioitoan@users.noreply.github.com>
2022-12-03 22:45:03 +0700

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Client } from "discord.js-selfbot-v13";
 import axios from "axios";
 import { config } from "dotenv";
-config()
+config();
 const client = new Client({ checkUpdate: false });
 client
     .on("ready", (c) => {
@@ -11,12 +11,17 @@ client
         if (message.author.id !== client.user?.id) return;
         if (message.content.startsWith("!upload")) {
             const args = message.content.split(" ").slice(1);
-            const desc = args[0];
-            const imageLink = args[1];
+            const a = args
+                .join(" ")
+                .match(
+                    /([^"])([|\p{L}|\p{Nd}|\s|!|#|$|%|&|'|(|)|*|+|,|-|.|/|:|;|<|=|>|?|@|[|\]|^|_|`|{|\||}|~]*)([^"])/gimu
+                );
+            const desc = a?.[0];
+            const imageLink = a?.[1];
             const m = await message.channel.send({
                 files: [
                     {
-                        attachment: imageLink,
+                        attachment: imageLink as string,
                         name: "chart.png",
                     },
                 ],
@@ -25,7 +30,7 @@ client
                 await m.delete();
             }, 1000);
             const originalImgLink = m.attachments.at(0)?.url;
-            const author = args[2];
+            const author = a?.[2];
             const url = `https://ablum-lalz.onrender.com`;
             var data = JSON.stringify({
                 imgLink: originalImgLink,
@@ -33,9 +38,9 @@ client
                 imgDescription: desc,
                 owner: {
                     name: author,
-                    service: 'idk',
-                    id: '0',
-                  },
+                    service: "idk",
+                    id: "0",
+                },
             });
 
             var config = {
@@ -48,13 +53,10 @@ client
             };
 
             axios(config)
-                .then(function (response) {
-                })
+                .then(function (response) {})
                 .catch(function (error) {
                     console.log(error);
                 });
         }
     })
-    .login(
-       process.env.c
-    );
+    .login(process.env.c);
