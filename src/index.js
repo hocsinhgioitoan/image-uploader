226147445c847b707a0fc7b503ad4e12b21504e8
hocsinhgioitoan <89916615+hocsinhgioitoan@users.noreply.github.com>
2022-12-04 14:25:21 +0700

    .

--- src/index.js
+++ src/index.js
@@ -3,62 +3,95 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_selfbot_v13_1 = require("discord.js-selfbot-v13");
+const discord_js_1 = require("discord.js");
 const axios_1 = __importDefault(require("axios"));
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
-const client = new discord_js_selfbot_v13_1.Client({ checkUpdate: false });
+const client = new discord_js_1.Client({
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
 client
     .on("ready", (c) => {
     console.log(`${c.user.username} is ready`);
+    c.guilds.cache.forEach((g) => {
+        g.commands.set([
+            {
+                name: "upload",
+                description: "hehe",
+            },
+        ]);
+    });
 })
-    .on("messageCreate", async (message) => {
-    if (message.author.id !== client.user?.id)
-        return;
-    if (message.content.startsWith("!upload")) {
-        const args = message.content.split(" ").slice(1);
-        const a = args
-            .join(" ")
-            .match(/([^"])([|\p{L}|\p{Nd}|\s|!|#|$|%|&|'|(|)|*|+|,|-|.|/|:|;|<|=|>|?|@|[|\]|^|_|`|{|\||}|~|-]*)([^"])/gimu);
-        const desc = a?.[0];
-        const imageLink = a?.[1];
-        const m = await message.channel.send({
-            files: [
-                {
-                    attachment: imageLink,
-                    name: "chart.png",
-                },
-            ],
-        });
-        setTimeout(async () => {
-            await m.delete();
-        }, 1000);
-        const originalImgLink = m.attachments.at(0)?.url;
-        const author = a?.[2];
-        const url = `https://ablum-lalz.onrender.com`;
-        var data = JSON.stringify({
-            imgLink: originalImgLink,
-            originalImgLink: imageLink,
-            imgDescription: desc,
-            owner: {
-                name: author,
-                service: "idk",
-                id: "0",
-            },
-        });
-        var config = {
-            method: "post",
-            url: `${url}/api/newpin`,
-            headers: {
-                "Content-Type": "application/json",
-            },
-            data: data,
-        };
-        (0, axios_1.default)(config)
-            .then(function (response) { })
-            .catch(function (error) {
-            console.log(error);
-        });
+    .on("interactionCreate", async (i) => {
+    if (i.isChatInputCommand()) {
+        const modal = new discord_js_1.ModalBuilder()
+            .setTitle("Upload ảnh")
+            .setCustomId("modal");
+        const op1 = new discord_js_1.TextInputBuilder()
+            .setCustomId("desc")
+            .setLabel("Mô tả")
+            .setStyle(discord_js_1.TextInputStyle.Short);
+        const op2 = new discord_js_1.TextInputBuilder()
+            .setCustomId("author")
+            .setLabel("Người up")
+            .setStyle(discord_js_1.TextInputStyle.Short);
+        const op3 = new discord_js_1.TextInputBuilder()
+            .setCustomId("link")
+            .setLabel("link ảnh")
+            .setStyle(discord_js_1.TextInputStyle.Short);
+        const row1 = new discord_js_1.ActionRowBuilder().addComponents(op1);
+        const row2 = new discord_js_1.ActionRowBuilder().addComponents(op2);
+        const row3 = new discord_js_1.ActionRowBuilder().addComponents(op3);
+        modal.addComponents(row1, row2, row3);
+        await i.showModal(modal);
+    }
+    else if (i.isModalSubmit()) {
+        if (i.customId === "modal") {
+            const desc = i.fields.getTextInputValue("desc");
+            const author = i.fields.getTextInputValue("author");
+            const link = i.fields.getTextInputValue("link");
+            try {
+                const m = await i.channel?.send({
+                    files: [
+                        {
+                            attachment: link,
+                        },
+                    ],
+                });
+                const imgLink = m?.attachments.at(0)?.url;
+                const url = `https://ablum-lalz.onrender.com`;
+                var data = JSON.stringify({
+                    imgLink: imgLink,
+                    originalImgLink: link,
+                    imgDescription: desc,
+                    owner: {
+                        name: author,
+                        service: "idk",
+                        id: "0",
+                    },
+                });
+                var config = {
+                    method: "post",
+                    url: `${url}/api/newpin`,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    data: data,
+                };
+                (0, axios_1.default)(config)
+                    .then(function (response) { });
+                i.reply({
+                    content: `done!`,
+                });
+                return;
+            }
+            catch (e) {
+                i.reply({
+                    content: `${e}`,
+                });
+                return;
+            }
+        }
     }
 })
     .login(process.env.c);
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,107 @@
-import { Client } from "discord.js-selfbot-v13";
+import {
+    ActionRowBuilder,
+    Client,
+    ModalActionRowComponentBuilder,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+} from "discord.js";
 import axios from "axios";
 import { config } from "dotenv";
 config();
-const client = new Client({ checkUpdate: false });
+const client = new Client({
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
 client
     .on("ready", (c) => {
         console.log(`${c.user.username} is ready`);
+        c.guilds.cache.forEach((g) => {
+            g.commands.set([
+                {
+                    name: "upload",
+                    description: "hehe",
+                },
+            ]);
+        });
     })
-    .on("messageCreate", async (message) => {
-        if (message.author.id !== client.user?.id) return;
-        if (message.content.startsWith("!upload")) {
-            const args = message.content.split(" ").slice(1);
-            const a = args
-                .join(" ")
-                .match(
-                    /([^"])([|\p{L}|\p{Nd}|\s|!|#|$|%|&|'|(|)|*|+|,|-|.|/|:|;|<|=|>|?|@|[|\]|^|_|`|{|\||}|~|-]*)([^"])/gimu
+    .on("interactionCreate", async (i) => {
+        if (i.isChatInputCommand()) {
+            const modal = new ModalBuilder()
+                .setTitle("Upload ảnh")
+                .setCustomId("modal");
+            const op1 = new TextInputBuilder()
+                .setCustomId("desc")
+                .setLabel("Mô tả")
+                .setStyle(TextInputStyle.Short);
+            const op2 = new TextInputBuilder()
+                .setCustomId("author")
+                .setLabel("Người up")
+                .setStyle(TextInputStyle.Short);
+            const op3 = new TextInputBuilder()
+                .setCustomId("link")
+                .setLabel("link ảnh")
+                .setStyle(TextInputStyle.Short);
+            const row1 =
+                new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+                    op1
                 );
-            const desc = a?.[0];
-            const imageLink = a?.[1];
-            const m = await message.channel.send({
-                files: [
-                    {
-                        attachment: imageLink as string,
-                        name: "chart.png",
-                    },
-                ],
-            });
-            setTimeout(async () => {
-                await m.delete();
-            }, 1000);
-            const originalImgLink = m.attachments.at(0)?.url;
-            const author = a?.[2];
-            const url = `https://ablum-lalz.onrender.com`;
-            var data = JSON.stringify({
-                imgLink: originalImgLink,
-                originalImgLink: imageLink,
-                imgDescription: desc,
-                owner: {
-                    name: author,
-                    service: "idk",
-                    id: "0",
-                },
-            });
-
-            var config = {
-                method: "post",
-                url: `${url}/api/newpin`,
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                data: data,
-            };
-
-            axios(config)
-                .then(function (response) {})
-                .catch(function (error) {
-                    console.log(error);
-                });
+            const row2 =
+                new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+                    op2
+                );
+            const row3 =
+                new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+                    op3
+                );
+            modal.addComponents(row1, row2, row3);
+            await i.showModal(modal);
+        } else if (i.isModalSubmit()) {
+            if (i.customId === "modal") {
+                const desc = i.fields.getTextInputValue("desc");
+                const author = i.fields.getTextInputValue("author");
+                const link = i.fields.getTextInputValue("link");
+                try {
+                    const m = await i.channel?.send({
+                        files: [
+                            {
+                                attachment: link,
+                            },
+                        ],
+                    });
+                    const imgLink = m?.attachments.at(0)?.url;
+                    const url = `https://ablum-lalz.onrender.com`;
+                    var data = JSON.stringify({
+                        imgLink: imgLink,
+                        originalImgLink: link,
+                        imgDescription: desc,
+                        owner: {
+                            name: author,
+                            service: "idk",
+                            id: "0",
+                        },
+                    });
+                    var config = {
+                        method: "post",
+                        url: `${url}/api/newpin`,
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        data: data,
+                    };
+                    axios(config)
+                        .then(function (response) {})
+                        i.reply({
+                            content: `done!`,
+                        });
+                        return
+                } catch (e) {
+                    i.reply({
+                        content: `${e}`,
+                    });
+                    return;
+                }
+            }
         }
     })
+
     .login(process.env.c);
